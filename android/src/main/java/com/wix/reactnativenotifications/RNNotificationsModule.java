8a74f9cc1bb0da2573e7d99ccbb32a7acfd03201
Louis Muk <louismukk@gmail.com>
2017-09-17 06:22:06 +0800

    dfd

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsModule.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
